d3b9c7c95a30d0053cd247d01c5d20a3fd46a832
Łukasz <43967017+magik9907@users.noreply.github.com>
2021-03-11 21:07:57 +0100

    league table

--- src/components/LeagueTable.tsx
+++ src/components/LeagueTable.tsx
@@ -1,6 +1,6 @@
 import { useEffect, useState } from 'react'
 import { Merge } from './MergeSort'
-
+import { Link } from 'react-router-dom'
 type jsonType = {
   idStanding?: string
   intRank?: string
@@ -23,13 +23,34 @@ type jsonType = {
   dateUpdated?: string
 }
 
-function Comparer(A: jsonType ={}, B: jsonType ={}): boolean {
-  if(!A.intRank)return false;
-  if(!B.intRank)return true;
+function Comparer(A: jsonType = {}, B: jsonType = {}): boolean {
+  if (!A.intRank) return false
+  if (!B.intRank) return true
   let rankA: number, rankB: number
-  rankA = parseInt(A["intRank"] || '0')
-  rankB = parseInt(B["intRank"] || '0')
+  rankA = parseInt(A['intRank'] || '0')
+  rankB = parseInt(B['intRank'] || '0')
   return rankA <= rankB
+}
+
+function TeamRank(props: jsonType) {
+  return (
+    <tr>
+      <td>{props.intRank}</td>
+      <td>
+        <Link to={'/team/' + props.idTeam}>
+          <img src={props.strTeamBadge} title={props.strTeam} alt="" />
+          {props.strTeam}
+        </Link>
+      </td>
+      <td>{props.intPoints}</td>
+      <td>{props.intWin}</td>
+      <td>{props.intDraw}</td>
+      <td>{props.intLoss}</td>
+      <td>{props.intGoalsFor}</td>
+      <td>{props.intGoalsAgainst}</td>
+      <td>{props.intGoalDifference}</td>
+    </tr>
+  )
 }
 
 const LeagueTable = (props: { leagueId: string }) => {
@@ -40,10 +61,8 @@ const LeagueTable = (props: { leagueId: string }) => {
     fetch(url)
       .then((response) => response.json())
       .then((data) => {
-        console.log(Merge<jsonType>( data.table, Comparer))
-        setSeasonJSON(
-          Merge<jsonType>( data.table, Comparer)
-        )
+        console.log(Merge<jsonType>(data.table, Comparer))
+        setSeasonJSON(Merge<jsonType>(data.table, Comparer))
       })
       .catch((error) => {
         console.error(error)
@@ -57,7 +76,26 @@ const LeagueTable = (props: { leagueId: string }) => {
       ) : (
         <>
           <p>{season}</p>
-          <table></table>
+          <table>
+            <thead>
+              <tr>
+                <td>position</td>
+                <td>team</td>
+                <td>points</td>
+                <td>win</td>
+                <td>draw</td>
+                <td>looses</td>
+                <td>goal for</td>
+                <td>goal against</td>
+                <td>goal difference</td>
+              </tr>
+            </thead>
+            <tbody>
+              {seasonJSON.map((rankPost) => (
+                <TeamRank key={rankPost.intRank} {...rankPost} />
+              ))}
+            </tbody>
+          </table>
         </>
       )}
     </div>
